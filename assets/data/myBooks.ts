5f35ff867de0c13735be463fe4a36f9e8274a4e7
Rajkaran12 <50668906+Rajkaran12@users.noreply.github.com>
2020-07-21 21:29:03 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] = [
+    {bname: 'Carrie', author: 'Stephen King', genre: 'Horror', pyear: 1974, picture: 'assets/images/carrie.jpg'},
+    {bname: 'The Night Fire', author: 'Michael Connelly', genre: 'Detective and Mystery', pyear: 2019, picture: 'assets/images/night_fire.jpg'},
+    {bname: 'Circe', author: 'Madeline Miller', genre: 'Fantasy', pyear: 2018, picture: 'assets/images/circe.jpg'},
+    {bname: 'The Help', author: ' Kathryn Stockett', genre: 'Historical Fiction', pyear: 2009, picture: 'assets/images/help.jpg'}
+    
+];
